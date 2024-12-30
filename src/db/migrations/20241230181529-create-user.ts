e14e3b68f436136a9ac74a6578a1431ce025ebc2
sodmaq <sodmaq@gmail.com>
2024-12-30 20:23:24 +0100

    migration file

---
+++ src/db/migrations/20241230181529-create-user.ts
@@ -0,0 +1,55 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+export async function up(queryInterface: QueryInterface): Promise<void> {
+  await queryInterface.createTable("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("active", "inactive"),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "user"),
+      allowNull: true,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
+
+export async function down(queryInterface: QueryInterface): Promise<void> {
+  await queryInterface.dropTable("users");
+}
