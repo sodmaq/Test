9e20fb42df4eaad3941cab5e9c42efdf714654a6
sodmaq <sodmaq@gmail.com>
2025-01-03 18:45:42 +0100

    user reg and login service

--- src/services/authService/index.ts
+++ src/services/authService/index.ts
@@ -1,4 +1,4 @@
-import { User, Auth } from "../../db/models";
+import { Profile, User, Auth } from "../../db/models";
 import { UserRole, UserStatus } from "../../utils/enums/user.enum";
 import {
   BadRequestError,
@@ -17,6 +17,83 @@ import { access } from "fs";
 class AuthService extends BaseService<Auth> {
   constructor() {
     super(Auth, "Auth");
+  }
+  public async register(data: RegisterUserData): Promise<User> {
+    const {
+      fullname,
+      username,
+      email,
+      phoneNumber,
+      password,
+      avatar,
+      bio,
+      facebook,
+      linkedIn,
+      x,
+      instagram,
+    } = data;
+    const whereOptions: { email: string; username?: string } = { email };
+    if (username) whereOptions.username = username;
+
+    const whereQuery = {
+      [Op.or]: [whereOptions],
+    };
+
+    // Check Email, Username, PhoneNumber
+    const userExists = await userService.get(whereQuery);
+    if (userExists) {
+      throw new BadRequestError("User already exists");
+    }
+    const attributes = {
+      fullname,
+      username,
+      email,
+      password,
+      avatar,
+      role: UserRole.user,
+      status: UserStatus.active,
+    };
+    // create user
+    const user = await User.create(attributes);
+
+    // Create Profile
+    await Profile.create({
+      userId: user.id,
+      bio,
+      facebook,
+      instagram,
+      x,
+      linkedIn,
+      phoneNumber,
+    });
+
+    await user.reload({ include: userService.defaultIncludeables(true) });
+
+    return user;
+  }
+  public async login(
+    user: User
+  ): Promise<{ accessToken: string; refreshToken: string; user: object }> {
+    if (user.status !== UserStatus.active) {
+      throw new UnauthorizedError(
+        "Your account is inactive. Please contact admin."
+      );
+    }
+    const userIdentity = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    };
+
+    // generate access token
+    const accessToken = jwtUtil.generate(userIdentity);
+    const refreshToken = jwtUtil.generate(userIdentity);
+
+    // reload user with includeables
+    await user.reload({ include: userService.defaultIncludeables(true) });
+    await this.createOrUpdate({ userId: user.id, isLoggedIn: true });
+    delete user.dataValues.password;
+    return { accessToken, refreshToken, user };
   }
 }
 
